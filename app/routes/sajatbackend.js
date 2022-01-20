fa2cacffdafc13bd88c4b308d4762561b3655a12
adamfaragoo <adam.farago004@gmail.com>
2022-01-20 09:28:03 +0100

    a

--- app/routes/sajatbackend.js
+++ app/routes/sajatbackend.js
@@ -1,8 +1,12 @@
 const { authJwt } = require("../middleware");
 const controller = require("../controllers/user.controller");
+const port = 3000
+
+
 
 module.exports = function(app) {
-  app.use(function(req, res, next) {
+  app.use(function(req, res, next)
+   {
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
@@ -10,6 +14,7 @@ module.exports = function(app) {
     next();
   });
 
+  
   app.get('/filmek', (req, res) => {
     var mysql = require('mysql')
     var connection = mysql.createConnection({
@@ -282,7 +287,9 @@ module.exports = function(app) {
   
   })
   
- 
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
   
   
   
